a733b9c123c1b9bca7cf7039f2df01f0854e2fe5
Aditya Kanase <110183779+adityakanase022@users.noreply.github.com>
2025-02-19 13:52:36 +0000

    added info

---
+++ part_1/01_text.js
@@ -0,0 +1,18 @@
+const accountId = 123434;  //--values can not be changed
+let acccountEmail = "adityakanase@gmail.com"; //access only inside brackets
+var accountPassowart = "pass"; // access anywhere in file (security issue) so not in use
+accountCity = "shenoli";// --> can be possible to decleare but not recommencded
+
+
+let accountState;
+console.log(accountState);
+
+// console.log(accountCity);
+
+/*
+this is multiline comment
+
+*/
+
+//below line will crete a table and give the indexing from 0 for each variable 
+console.table([accountId,acccountEmail,accountCity]);
---
+++ part_1/02_text.js
@@ -0,0 +1,21 @@
+// "use strict"; 
+//treat all js files as newer version
+//can not use because this is not a browser this is node js ,their is another syntax
+
+// code readability is should be high
+
+/*
+nubmer => 2^53
+bigint
+string ="";
+boolean -> true/false
+null -> standalone value /null means empty
+undefined -
+symbol => most used inside react /unique 
+
+
+object
+
+*/
+
+console.log(typeof("aditya"));
